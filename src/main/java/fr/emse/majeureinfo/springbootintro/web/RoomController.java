100f6a32ce8f88cc2ab300f016e81e50f9693d8a
SuperValou96 <valentin.pouzet@etu.emse.fr>
2017-11-14 16:11:26 +0100

    feat : switchRinger en post

--- src/main/java/fr/emse/majeureinfo/springbootintro/web/RoomController.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/web/RoomController.java
@@ -2,10 +2,7 @@ package fr.emse.majeureinfo.springbootintro.web;
 
 import fr.emse.majeureinfo.springbootintro.dao.RoomDao;
 import fr.emse.majeureinfo.springbootintro.model.Room;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -28,7 +25,7 @@ public class RoomController {
         return roomDao.findAll().stream().map(RoomDto::new).collect(Collectors.toList());
     }
 
-    @GetMapping(value = "/{roomId}/switchRinger")
+    @PostMapping(value = "/{roomId}/switchRinger") //On travaille en post pour pouvoir changer dans la BDD
     public RoomDto switchRinger(@PathVariable Long roomId) {
         Room myRoom = roomDao.findOne(roomId) ; // On récupère la room
         myRoom.switchRinger(); // On switch
