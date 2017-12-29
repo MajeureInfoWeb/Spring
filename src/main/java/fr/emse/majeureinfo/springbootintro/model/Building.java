2fc97668580213013c3da854560c4576a9a947ac
SuperValou96 <valentin.pouzet@etu.emse.fr>
2017-12-29 17:54:24 +0100

    feat- ENFIN UN BUILDING QUI MARCHE ! :D

--- src/main/java/fr/emse/majeureinfo/springbootintro/model/Building.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/model/Building.java
@@ -3,6 +3,7 @@ package fr.emse.majeureinfo.springbootintro.model;
 import org.springframework.data.annotation.Persistent;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @SuppressWarnings("serial")
@@ -11,42 +12,38 @@ public class Building {
 
     @Id
     @GeneratedValue
-    private Long id;
-
-    // Bon ça marche même si je ne mets pas d'arobase... (Je me suis pris la tête alors qu'il me manquait juste une virgule dans ma requête)
-    private String nom;
     private Long buildingid;
+    //un seul id est nécessaire
+    // En revanche on est obligé d'avoir une table avec le nom des buildings
+    // et une autre table de join où on dit lesquels ont quels rooms
 
+    // Crée la colonne dans la BDD (mais ça marche même si je ne le mets pas)
+    @Column(nullable = false)
+    private String nom;
 
-    //@Column(nullable = false)
+    /**
+     * One of the Room of a building
+     */
+    // je sais pas trop ce que fait le mappedBy mais j'avais noté ça ...
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Room> rooms;
+
 
     @SuppressWarnings("unused")
     public Building() {
     }
 
-    public Building(Room room) {
-        this.room = room;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-    public void setId(Long id) {
-        this.id = id;
+    public Building(List<Room> rooms, String nom) {
+        // Un building, c'est DES rooms et un nom
+        this.rooms = rooms;
+        this.nom = nom;
     }
 
     public Long getBuildingId() {
         return this.buildingid;
     }
     public void setBuildingId(Long id) {
-        this.buildingid = buildingid;
-    }
-
-    public Room getRoom() {
-        return room;
-    }
-    public void setRoom(Room room) {
-        this.room = room;
+        this.buildingid = id;
     }
 
     public String getNom() {
@@ -56,12 +53,11 @@ public class Building {
         this.nom = nom;
     }
 
-
-    /**
-     * The Room of a building
-     */
-    // je sais pas encore bien ce que ça fait vu que ça marchait avec le onetoone
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private Room room;
+    public List<Room> getRooms() {
+        return rooms;
+    }
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
 
 }
--- src/main/java/fr/emse/majeureinfo/springbootintro/web/BuildingDto.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/web/BuildingDto.java
@@ -5,29 +5,42 @@ import fr.emse.majeureinfo.springbootintro.model.Light;
 import fr.emse.majeureinfo.springbootintro.model.Noise;
 import fr.emse.majeureinfo.springbootintro.model.Room;
 
+import java.util.List;
+
 public class BuildingDto {
 
-    //private final Long id;
     private final Long buildingid;
-    private final RoomDto room;
+    private final String nom;
+    private final List<Room> rooms;
+    // dans room on prenait les dto des light et tout
+    // mais on n'en a pas besoin là
+    // private RoomDto room;
 
 
     public BuildingDto(Building building) {
-        //this.id = building.getId();
         this.buildingid = building.getBuildingId();
-        this.room = new RoomDto(building.getRoom());
+        this.nom = building.getNom();
+        /*List<Room> maliste = building.getRooms();
+        for (int i = 0; i < maliste.size(); i++){
+            this.rooms.add(new RoomDto(maliste.get(i)));
+        }
+        J'ai pas besoin d'une liste de dto, c'est trop compliqué
+        */
+        this.rooms = building.getRooms();
     }
 
-    /*public Long getId() {
-        return id;
-    }*/
     public Long getBuildingId() {
         return buildingid;
     }
-
-    public RoomDto getRoom() {
-        return room;
+    public List<Room> getRooms() {
+        return rooms;
     }
+    /*public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+        il veut pas psk genre c'est final
 
+        je crois que je n'ai pas tout à fait résolu mon dto ...
+        */
 
 }
